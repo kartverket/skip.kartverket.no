3a6fb62d8f4e0ccc7f2fa6ddf56d6c082e600730
Espen Henriksen <espen.henriksen@kartverket.no>
2021-09-23 11:31:08 +0200

    Replace changelog with blog

---
+++ blog/2021-09-23/sprint-14.md
@@ -0,0 +1,27 @@
+---
+title: Prosjektstatus sprint 14
+description: Målsetning for sprint 14
+slug: sprint-14
+authors: espen
+tags: [skip, sprint]
+hide_table_of_contents: false
+---
+
+Velkommen til SKIP-bloggen! Her kommer vi til å kommunisere signifikante
+endringer i platformen samt våre fokusområder fremover.
+
+Her følger teamets målsetning for sprint 14 (Sep 16, 2021–Sep 30, 2021).
+
+<!--truncate-->
+
+I sprint 14 skal vi fokusere på følgende delmål:
+
+- Vi setter opp nettside for dokumentasjon og kommunikasjon mot resten av
+  organisasjonen (den du leser dette på nå!)
+- Vi etablerer velero som backup-system
+- Vi ferdigstiller ELK-stack for logging av applikasjon
+- Vi får på plass terraform for å bevege platformen nærmere infrastructure as
+  code og GitOps
+- Vi jobber videre med Vault
+- Vi kartlegger, jobber med skystrategi og dokumenterer oppsett
+- Vi jobber kontinuerlig med å stabilisere clusteret
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,109 +1,119 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'SKIP',
-  tagline: 'Statens Kartverks Infrastrukturplatform',
-  url: 'https://skip.pages.statkart.no',
-  baseUrl: '/docs/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon/favicon.ico',
-  organizationName: 'skip', // Usually your GitHub org/user name.
-  projectName: 'SKIP', // Usually your repo name.
+(
+  module.exports = {
+    title: "SKIP",
+    tagline: "Statens Kartverks Infrastrukturplatform",
+    url: "https://skip.pages.statkart.no",
+    baseUrl: "/docs/",
+    onBrokenLinks: "warn",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon/favicon.ico",
+    organizationName: "skip", // Usually your GitHub org/user name.
+    projectName: "SKIP", // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://gitlab.statkart.no/skip/docs/-/edit/master/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            editUrl: "https://gitlab.statkart.no/skip/docs/-/edit/master/",
+          },
+          blog: {
+            blogTitle: '',
+            blogDescription: 'A Docusaurus powered blog!',
+            blogSidebarTitle: 'Siste poster',
+            feedOptions: {
+              type: "all",
+              copyright: `Copyright © ${new Date().getFullYear()} Statens Kartverk`,
+            },
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
     ],
-  ],
 
-  themeConfig: {
-    navbar: {
-      title: 'SKIP',
-      logo: {
-        alt: 'SKIP',
-        src: 'img/skip.png',
+    themeConfig: {
+      navbar: {
+        title: "SKIP",
+        logo: {
+          alt: "SKIP",
+          src: "img/skip.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            type: "doc",
+            docId: "roadmap",
+            position: "left",
+            label: "Roadmap",
+          },
+          {
+            to: "blog",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            href: "https://confluence.statkart.no/display/SKIP",
+            label: "Confluence",
+            position: "right",
+          },
+          {
+            href: "https://gitlab.statkart.no/skip",
+            label: "GitLab",
+            position: "right",
+          },
+        ],
       },
-      items: [
-        {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Docs',
-        },
-        {
-          type: 'doc',
-          docId: 'roadmap',
-          position: 'left',
-          label: 'Roadmap',
-        },
-        {
-          type: 'doc',
-          docId: 'changelog',
-          position: 'left',
-          label: 'Changelog',
-        },
-        {
-          href: 'https://confluence.statkart.no/display/SKIP',
-          label: 'Confluence',
-          position: 'right',
-        },
-        {
-          href: 'https://gitlab.statkart.no/skip',
-          label: 'GitLab',
-          position: 'right',
-        },
-      ],
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Introduction",
+                to: "/docs/docs/intro",
+              },
+              {
+                label: "Roadmap",
+                to: "/docs/docs/roadmap",
+              },
+              {
+                label: "Blog",
+                to: "/docs/blog",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitLab",
+                href: "https://gitlab.statkart.no/skip",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Statens Kartverk. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["java"],
+      },
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Introduction',
-              to: '/docs/docs/intro',
-            },
-            {
-              label: 'Roadmap',
-              to: '/docs/docs/roadmap',
-            },
-            {
-              label: 'Changelog',
-              to: '/docs/changelog',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitLab',
-              href: 'https://gitlab.statkart.no/skip',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Statens Kartverk. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-      additionalLanguages: ['java'],
-    },
-  },
-});
+  }
+);
