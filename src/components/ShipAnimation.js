4f03cc0a86c4947e8f3bef1b4a3c3597904531b0
Espen Henriksen <espen.henriksen@kartverket.no>
2022-12-20 12:18:20 +0100

    Add a little easter egg

--- src/components/ShipAnimation.js
+++ src/components/ShipAnimation.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './ShipAnimation.module.css';
 import ship from '../../static/img/ship.png';
 
 export default function ShipAnimation() {
-  return <img src={ship} className={styles.ship} />
+  const [clickCount, setClickCount] = useState(0);
+  const handleClick = () => {
+    setClickCount(val => val + 1);
+  };
+  useEffect(() => {
+    const amountOfTeamMembersInTheGoldenDays = 12;
+    if (clickCount === amountOfTeamMembersInTheGoldenDays) {
+      window.location.pathname = '/img/dreamteam.png';
+    }
+  }, [clickCount]);
+
+  return <img src={ship} className={styles.ship} onClick={handleClick} />;
 }
